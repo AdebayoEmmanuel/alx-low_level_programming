00a1dd95c512c4186b8ad7e4aed07476cbd2d6ec
Emmanuel Adebayo <adebayodemilade7@gmail.com>
2022-03-11 23:46:01 +0100

    CREATE 4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+/**
+*main - Entry point
+*
+*Return: Always 0 (Success)
+*/
+#include <stdio.h>
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle.\n");
+	return (0);
+}
