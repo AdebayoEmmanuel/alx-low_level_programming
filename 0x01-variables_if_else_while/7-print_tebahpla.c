115884238bf76e728fb6568a944588b5af4a9975
Emmanuel Adebayo <adebayodemilade7@gmail.com>
2022-03-13 05:26:30 +0100

    CREATE 7-tebahpla

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+* main - Entry point
+*
+*
+* Return: Always 0 (Success)
+*/
+int main(void)
+{
+	char x;
+
+for (x = 'z' ; x >= 'a' ; x--)
+{
+	putchar(x);
+}
+	putchar('\n');
+	return (0);
+}
