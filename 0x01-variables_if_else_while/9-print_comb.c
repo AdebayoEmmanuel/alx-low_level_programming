4e74dfc7d29b6aa73919e361b95f03b767b43928
Emmanuel Adebayo <adebayodemilade7@gmail.com>
2022-03-13 06:36:52 +0100

    CREATE 9-print_comb

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/**
+* main - Entry point
+*
+*
+* Return: Always 0 (Success)
+*/
+int main(void)
+{
+	char x;
+
+for (x = '0' ; x <= '9' ; x++)
+{
+	putchar(x);
+if (x != '9')
+{
+	putchar(',');
+	putchar(' ');
+}
+}
+	putchar('\n');
+	return (0);
+}
