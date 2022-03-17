d2f4d4d0571091ea5ddf4251d07f7ed09829779d
Emmanuel Adebayo <adebayodemilade7@gmail.com>
2022-03-17 01:41:33 +0100

    I sometimes suffer from insomnia. And when I can't fall asleep

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+* print_alphabet - prints all the english alphabets
+* in lower case
+*
+*Return: 0
+*/
+void print_alphabet(void)
+{
+	char i;
+
+	for (i = 'a'; i <= 'z'; i++)
+{
+	_putchar(i);
+}
+	_putchar('\n');
+}
