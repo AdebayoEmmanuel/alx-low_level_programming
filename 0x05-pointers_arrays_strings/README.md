422103d52e1d42c45c8615f18fb793bc5b596e65
Emmanuel Adebayo <adebayodemilade7@gmail.com>
2022-03-23 03:34:28 +0100

    RENAME 0x03-debugging

---
+++ 0x05-pointers_arrays_strings/README.md
@@ -0,0 +1,1 @@
+This directory contains files for the ALX tasks on pointers, arrays and strings
---
+++ 0x05-pointers_arrays_strings/main.h
@@ -0,0 +1,6 @@
+#ifndef MAIN_H_INCLUDED
+#define MAIN_H_INCLUDED
+
+char *_strcat(char *dest, char *src);
+
+#endif
