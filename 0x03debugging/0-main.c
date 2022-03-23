987fc00247ffbc002b72c7f3c5579029d94f97ff
Emmanuel Adebayo <adebayodemilade7@gmail.com>
2022-03-23 03:32:23 +0100

    CREATE 0-main.c && main.h

---
+++ 0x03debugging/0-main.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+* main - tests function that prints if integer is +ve or -ve
+* Return: 0
+*/
+
+intr main(void)
+{
+	int i;
+
+	i = 0;
+	positive_or_negative(i);
+
+	return (0);
+}
---
+++ 0x03debugging/README.md
@@ -0,0 +1,1 @@
+This directory contains all the working files for ALX C debugging tasks
