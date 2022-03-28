0ea5a26f2ee39afd4ee2f7debb1ca0e49cff4508
Emmanuel Adebayo <adebayodemilade7@gmail.com>
2022-03-28 01:10:09 +0100

    CREATE 1-isdigit.c

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+* _isupper - checks if a character is digit 0-9 or not.
+* @c: character to be checked
+* Return: 1 if digit 0-9, 0 otherwise.
+*
+*/
+int _isdigit(int c)
+{
+	return (c >= '0' && c <= '9');
+}
