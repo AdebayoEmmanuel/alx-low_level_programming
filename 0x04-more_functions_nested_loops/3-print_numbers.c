65d0e37af8cad4b90c7f23013f31951e15d0a542
Emmanuel Adebayo <adebayodemilade7@gmail.com>
2022-03-28 01:53:36 +0100

    CREATE 3-print_numbers.c

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+* print_numbers - prints numbers from 0-9
+*
+*
+*/
+void print_numbers(void)
+{
+	int x = '0';
+
+	while (x <= '9')
+	{
+	_putchar(x);
+	x++;
+	}
+	_putchar('\n');
+}
