a0d6bc8ed6121081cbcfe0773f2d3d0f9619f02f
Emmanuel Adebayo <adebayodemilade7@gmail.com>
2022-03-28 01:22:02 +0100

    CREATE 2-mul.c

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+* mul - multiplies two integers
+*
+* @a: first integer
+*
+* @b: second integer
+*
+* Return: 0 (Successful)
+*/
+int mul(int a, int b)
+{
+	int x = a * b;
+
+	_putchar(x);
+	return (0);
+}
