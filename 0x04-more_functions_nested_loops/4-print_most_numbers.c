c88d492c5f8f4eccb1175b83a1d13c42c93dcaaa
Emmanuel Adebayo <adebayodemilade7@gmail.com>
2022-03-28 02:25:30 +0100

    CREATE 4-print_most_numbers.c

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -10,12 +10,11 @@ void print_most_numbers(void)
 
 	while (x <= '9')
 	{
-	if (x == '2' || x == '4')
+	if (x != '2' || x != '4')
 	{
-	continue;
-	}
 	_putchar(x);
 	x++;
 	}
 	_putchar('\n');
+}
 }
