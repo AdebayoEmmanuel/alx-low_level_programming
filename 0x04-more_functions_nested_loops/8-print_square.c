13dbfb4282ac126f73289302e1b10dbe0cd5df7d
Emmanuel Adebayo <adebayodemilade7@gmail.com>
2022-04-05 03:18:11 +0100

    MODIFY 8-print_square.c

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -1,28 +1,27 @@
 #include "main.h"
-/*
-* print_square - prints a square of dimension size x size
-*
-* @i : tracker for new line
-* @j : tracker for # printing
-*/
+
+/**
+ * print_square - prints a square
+ * @size: the size of the square
+ */
+
 void print_square(int size)
 {
-	int i, j;
+	int x, y;
 
-	if (size <= 0)
+	if (size > 0)
 	{
-		_putchar('\n');
+		for (x = 0; x < size; x++)
+		{
+			for (y = 0; y < size; y++)
+			{
+				_putchar(35);
+			}
+			_putchar(10);
+		}
 	}
 	else
 	{
-		for (i = 0; i < size; i++)
-		{
-			_putchar('\n');
-			for (j = 0; j < size; j++)
-			{
-				_putchar('#');
-			}
-		}
+		_putchar(10);
 	}
-
 }
