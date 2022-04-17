a74966e88da90962750b0d2d37d30fdc842bfe86
Emmanuel Adebayo <adebayodemilade7@gmail.com>
2022-04-17 04:26:41 +0100

    UPDATE 2-print_dog.c

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -11,21 +11,13 @@ void print_dog(struct dog *d)
 	if (d != 0)
 	{
 		if (d->name != 0)
-		{
 			printf("Name: %s\n", d->name);
-		}
 		else
-		{
 			printf("Name: (nil)\n");
-		}
 		printf("Age: %f\n", d->age);
 		if (d->owner != 0)
-		{
 			printf("Owner: %s\n", d->owner);
-		}
 		else
-		{
-			printf("owner: (nil)\n");
-		}
+			printf("Owner: (nil)\n");
 	}
 }
