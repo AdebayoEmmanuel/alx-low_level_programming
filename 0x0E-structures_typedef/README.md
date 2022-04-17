27233fce97f7c0091228169d6076d764b4365165
Emmanuel Adebayo <adebayodemilade7@gmail.com>
2022-04-17 03:15:33 +0100

    CREATE dog.h && README.md

---
+++ 0x0E-structures_typedef/README.md
@@ -0,0 +1,7 @@
+# This repository contains files and directories for the 0x0E. C - Structures, typedef 
+# tasks for the ALX SE program.
+
+0. Poppy : Define a new type struct dog with the following elements:
+* name, type = char *
+* age, type = float
+* owner, type = char *
---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,20 @@
+#ifndef dog_h
+#define dog_h
+/**
+* struct dog - a dog structure, with some attributes
+*
+* @name: dog name
+* @age: dog age
+* @owner: dog owner
+*
+* Description: This struct is the solution to 0. Poppy task
+*
+*/
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+#endif
