6f22261fb979ea9da213e32da7ae7729fb4d6e1d
Emmanuel Adebayo <adebayodemilade7@gmail.com>
2022-04-17 03:35:35 +0100

    CREATE 1-init_dog.c

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include "dog.h"
+/**
+* init_dog - a function that initializes a variable of type struct dog
+* @d: new dog struct
+* @name: name parameter for the new dog struct
+* @age: age parameter for the new dog struct
+* @owner: owner parameter for the new dog struct
+*
+*/
+
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	d->name = name;
+	(*d).age = age;
+	d->owner = owner;
+}
--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -17,4 +17,6 @@ struct dog
 	float age;
 	char *owner;
 };
+
+void init_dog(struct dog *d, char *name, float age, char *owner);
 #endif
