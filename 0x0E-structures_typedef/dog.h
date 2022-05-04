f3dd13c1065fac02511bd7fd1c5a27b6da4775a3
Emmanuel Adebayo <adebayodemilade7@gmail.com>
2022-05-04 20:52:23 +0000

    UPDATE dog.h

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -21,5 +21,6 @@ typedef struct dog dog_t;
 
 dog_t *new_dog(char *name, float age, char *owner);
 void print_dog(struct dog *d);
+void free_dog(dog_t *d);
 void init_dog(struct dog *d, char *name, float age, char *owner);
 #endif
