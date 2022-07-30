18d516bfc7f4df8e46e63e60aab6e2e153c06882
Emmanuel Adebayo <adebayodemilade7@gmail.com>
2022-07-30 01:56:59 +0000

    ADD test

---
+++ 0x01A-hash_tables/0-hash_table_create.c
@@ -0,0 +1,13 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_create - creates a new hash table
+ * @size: size of hash table array
+ *
+ * Return: ponter to new hash table
+ * returns NULL on failure
+ */
+hash_table_t *hash_table_create(unsigned long int size)
+{
+	hash_table *ht;	
+}
---
+++ 0x01A-hash_tables/hash_tables.h
@@ -0,0 +1,6 @@
+#ifndef HASH_TABLES_H
+#define HASH_TABLES_H
+
+hash_table_t *hash_table_create(unsigned long int size)
+
+#endif
