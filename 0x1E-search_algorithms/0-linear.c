0013b0fda9cd46de1460506802edcb62f0c53e5b
Emmanuel <adebayodemilade7@gmail.com>
2023-01-27 09:34:16 +0000

    BETTY compliance

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -13,11 +13,11 @@
 
 int linear_search(int *array, size_t size, int value)
 {
-	int i = 0;
+	int i;
 
 	if (array != NULL)
 	{
-		for (; i <= (int)size - 1; i++)
+		for (i = 0; i <= (int)size - 1; i++)
 		{
 			if (array[i] == value)
 			{
