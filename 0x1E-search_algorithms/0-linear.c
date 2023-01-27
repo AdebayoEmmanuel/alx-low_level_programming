a65463a3dac93779ad4d8e24999de75efc2f53ef
Emmanuel <adebayodemilade7@gmail.com>
2023-01-27 09:26:26 +0000

    BETTY compliance

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -1,24 +1,36 @@
 #include <stdio.h>
 #include "search_algos.h"
 
-/*linear search algorithm
- * uses O(n) complecity
+/*
+ * linear_search - takes array of size and finds value from it
+ * @*array: array of integers
+ * @size: size of array
+ * @value: value to search
+ * * Description: Does my stuff in O(n).
+ * Return: index of value or -1
  */
 
-int linear_search(int *array, size_t size, int value){
+int linear_search(int *array, size_t size, int value)
+{
 	int i = 0;
-	if(array != NULL){
-		for(; i <= (int)size-1; i++ ){
-			if(array[i] == value){
-				return i;
+
+	if (array != NULL)
+	{
+		for (; i <= (int)size - 1; i++)
+		{
+			if (array[i] == value)
+			{
+				return (i);
 			}
-			if(array[i] != value){
+			if (array[i] != value)
+			{
 				printf("Value checked array[%i] = [%i]\n", i, array[i]);
 			}
-			if(i == (int)size){
-				return -1;
+			if (i == (int)size)
+			{
+				return (-1);
 			}
 		}
 	}
-	return -1;
+	return (-1);
 }
